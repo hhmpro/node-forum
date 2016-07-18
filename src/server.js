d4bf743778bb77a804ddce421f067149cd80773e
hhmpro <240050497@qq.com>
2016-07-18 17:55:01 +0800

    实现用户基本功能

--- src/server.js
+++ src/server.js
@@ -6,18 +6,18 @@ import createDebug from 'debug';
 const $ = global.$ = new ProjectCore();
 
 // 创建debug函数
-$.createDebug = function(name){
+$.createDebug = function(name) {
   return createDebug('my:' + name);
 };
 const debug = $.createDebug('server');
 
 // 加载配置文件
-$.init.add((done)=>{
+$.init.add((done) => {
   $.config.load(path.resolve(__dirname, 'config.js'));
   const env = process.env.NODE_ENV || null;
-  if(env){
-    debug('load env : %s',env);
-    $.config.load(path.resolve(__dirname,'../config',env + '.js'));
+  if (env) {
+    debug('load env : %s', env);
+    $.config.load(path.resolve(__dirname, '../config', env + '.js'));
   }
   $.env = env;
   done();
@@ -29,18 +29,23 @@ $.init.load(path.resolve(__dirname, 'init', 'mongodb.js'));
 // 加载Models
 $.init.load(path.resolve(__dirname, 'models'));
 
+// 加载methods
+$.init.load(path.resolve(__dirname, 'methods'));
+
 // 初始化Express
 $.init.load(path.resolve(__dirname, 'init', 'express.js'));
 // 加载路由
 $.init.load(path.resolve(__dirname, 'routes'))
 
 // 初始化
-$.init((err)=>{
-  if(err){
+$.init((err) => {
+  if (err) {
     console.error(err);
     process.exit(-1);
-  }else{
-    console.log('inited [env=%s]',$.env);
+  } else {
+    console.log('inited [env=%s]', $.env);
   }
+
+  require('./test');
 
 })
